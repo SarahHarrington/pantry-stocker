66427a88e632e2d4a8a11a47b9187abbca61eb3b
Sarah Harrington <Sarah.Soberg@gmail.com>
2017-11-17 16:56:42 -0600

    inputs on add items available, add item controller started

--- server/public/scripts/controllers/additems.controller.js
+++ server/public/scripts/controllers/additems.controller.js
@@ -2,4 +2,14 @@ myApp.controller('AddItemController', function (UserService) {
     console.log('AddItemController created');
     var vm = this;
     vm.userService = UserService;
+
+    var item = {
+        itemName: '',
+        itemQuantity: '',
+        storePurchased: '',
+        defaultStore: '',
+        expirationDate: '',
+        reminderQty: ''
+    }
+
 });
