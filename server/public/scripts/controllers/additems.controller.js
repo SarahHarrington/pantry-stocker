fac2b84d5d67f0bbc90c6edb0dfa7e84235c892f
Sarah Harrington <Sarah.Soberg@gmail.com>
2017-11-17 23:56:57 -0600

    add item route partially working

--- server/public/scripts/controllers/additems.controller.js
+++ server/public/scripts/controllers/additems.controller.js
@@ -2,9 +2,7 @@ myApp.controller('AddItemController', function (UserService, AddItemService) {
     console.log('AddItemController created');
     var vm = this;
 
-    
-
     vm.addItem = function (newItem) {
-        AddItemService.addItem(newItem);
+        AddItemService.addItem();
     }
 });
--- server/routes/additems.router.js
+++ server/routes/additems.router.js
@@ -2,33 +2,52 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var path = require('path');
+var pool = require('../modules/pool.js');
 var pg = require('pg');
 
 
 //router to add item
 router.post('/', function (req, res) {
-    if(req.isAuthenticated()) {
+    if (req.isAuthenticated()) {
         console.log('add item post');
-        var userInfo = req.user._id;
-        console.log('userid', req.user._id);
-        
-    //    var queryText = 'INSERT INTO "items" '
-    //     db.query(queryText, [], function(errorMakingQuery, result){
-    //         if(errorMakingQuery) {
-    //             console.log('Error Making Query - Add Item', errorMakingQuery);
-    //             res.sendStatus(500);
-    //         }//if query error
-    //         else{
-    //             res.sendStatus(201);
-    //         }//if query else
-    //     })//db.query
-    // }//if authenticated
-    // else {
-    //     console.log('User is not logged in');
-    //     res.send(false);
+        var userInfo = req.user.id;
+        var item = req.body;
+        console.log('userid', req.user.id)
+        console.log(req.body);
+        pool.connect(function (errorConnectingtoDB, db, done) {
+            if (errorConnectingtoDB) {
+                console.log('Error Connecting to DB', errorConnectingtoDB);
+                res.sendStatus(500);
+            }
+            else {
+                db.query('INSERT INTO "Items" ("item_name", "user_id") VALUES ($1, $2) RETURNING "item_id";',
+                    [item.itemName, userInfo], function (errorMakingQuery, result) {
+                        if (errorMakingQuery) {
+                            console.log('Error Making Query - Add Item', errorMakingQuery);
+                            res.sendStatus(500);
+                        }//if query error for item insert in item table
+                        else {                            
+                            db.query('INSERT INTO "stock" ("item_id", "quantity", "min_quantity") VALUES ($1, $2, $3);',
+                                [result.rows[0].item_id, item.itemQuantity, item.reminderQuantity], function (errorMakingQuery, result) {
+                                    done();
+                                    if (errorMakingQuery) {
+                                        console.log('Error Making Query - Add Item to Stock', errorMakingQuery);
+                                        res.sendStatus(500);
+                                    }//if for item stock insert
+                                    else {
+                                        res.sendStatus(201);
+                                    }//item stock insert
+                                })//db.query 
+                        }//if query else   
+                    })//db.query
+            }//end else for pool
+        })//end pool
+    }//if authenticated
+    else {
+        console.log('User is not logged in');
+        res.send(false);
     }//is authenticated
 });//router.post for add item
-
 
 
 module.exports = router;
