eb3ad8455e7ac883b26f159a04c48e3810100f66
Sarah Harrington <Sarah.Soberg@gmail.com>
2017-11-18 22:12:29 -0600

    user registration updates

--- server/public/scripts/controllers/login.controller.js
+++ server/public/scripts/controllers/login.controller.js
@@ -2,8 +2,11 @@ myApp.controller('LoginController', function($http, $location, UserService) {
     console.log('LoginController created');
     var vm = this;
     vm.user = {
+      name: '',
       username: '',
-      password: ''
+      password: '',
+      email: ''
+
     };
     vm.message = '';
 
