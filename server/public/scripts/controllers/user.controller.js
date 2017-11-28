3733aa6af8118426cca07247cd128f3b5eb363e4
Sarah Harrington <Sarah.Soberg@gmail.com>
2017-11-28 12:17:40 -0600

    formatting updates

--- server/public/scripts/controllers/user.controller.js
+++ server/public/scripts/controllers/user.controller.js
@@ -1,6 +1,15 @@
-myApp.controller('UserController', function(UserService) {
+myApp.controller('UserController', function(UserService, $location) {
   console.log('UserController created');
   var vm = this;
   vm.userService = UserService;
   vm.userObject = UserService.userObject;
+
+  vm.isOpen = false;
+
+  vm.demo = {
+    isOpen: false,
+    count: 0,
+    selectedDirection: 'right'
+  };
+
 });
