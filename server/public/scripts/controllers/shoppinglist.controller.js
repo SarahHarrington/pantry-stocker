9aac66fe735938d3e138efec194de19e14806f4d
Sarah Harrington <Sarah.Soberg@gmail.com>
2017-11-30 17:35:42 -0600

    updated styling

--- server/public/scripts/controllers/shoppinglist.controller.js
+++ server/public/scripts/controllers/shoppinglist.controller.js
@@ -49,9 +49,11 @@ myApp.controller('ShoppingListController', function (UserService, AddItemService
     vm.deleteItemFromList= function(item) {
         console.log('delete from list', item);
         var itemId = item.item_id;
-        var store = {
-            store_id: item.store_id
-        };
+        // var store = {
+        //     store_id: item.store_id
+        // };
+
+        var store = item.store_id;
         ShoppingListService.deleteItemFromList(item).then(function (response) {
             vm.getShoppingLists(store); 
         })
