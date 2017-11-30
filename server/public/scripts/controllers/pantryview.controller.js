3bf3362bed3b2c7efd54edf09bc3e9279218b7e6
Sarah Harrington <Sarah.Soberg@gmail.com>
2017-11-30 11:05:25 -0600

    organized code and moved some things from controllers to services

--- server/public/scripts/controllers/pantryview.controller.js
+++ server/public/scripts/controllers/pantryview.controller.js
@@ -26,78 +26,33 @@ myApp.controller('PantryViewController', function (UserService, UserSetupService
         })
     }
 
+    //removes one item fromt he pantry
     vm.removeOneItem = function(item) {
         var itemId = item.item_id;
         var pantryId = item.pantry_location;
         AddItemService.removeOneItem(item).then(function(response){
             vm.getPantryItems({ pantry_id: pantryId })
-            // vm.openToast();
             vm.verifyItemShopList(itemId);
         })
     }
 
+    //adds one item to the pantry
     vm.addOneItem = function(item) {
         var itemId = item.item_id;
         var pantryId = item.pantry_location;
         AddItemService.addOneItem(item).then(function (response) {
             vm.getPantryItems({ pantry_id: pantryId });
-            // vm.openToast();
             vm.verifyItemShopList(itemId);
         })
     }
 
-    vm.openToast = function ($event) {
-        $mdToast.show(
-            $mdToast.simple()
-            .textContent('Your item has been updated!')
-            .hideDelay(1000));
-    }
-
     vm.shoppingListItemId = AddItemService.shoppingListItemId;
+    //checks to see if item meets threshold for minimum qty, if so does popup
     vm.verifyItemReminder = function (itemId) {
-        AddItemService.verifyItemReminder(itemId)
-            .then(function (response) {
-                var shoppingListItemId = response.itemId;
-                vm.shoppingListItemId = shoppingListItemId;
-                var response = response.response;
-                for (var i = 0; i < response.length; i++) {
-                    var totalItemQuantity = Number.parseInt(response[i].total_quantity);
-                    var minItemQuantity = response[i].min_quantity;
-                    if (totalItemQuantity <= minItemQuantity) {
-                        vm.showConfirm(shoppingListItemId);
-                    }
-                }
-            })
+        AddItemService.verifyItemReminder(itemId);
     }
 
-    vm.showConfirm = function (shoppingListItemId) {
-        $mdDialog.show({
-            controller: 'AddItemController as aic',
-            templateUrl: 'views/templates/addtoshoppinglist.html',
-            parent: angular.element(document.body),
-            targetEvent: shoppingListItemId,
-            clickOutsideToClose: true,
-            fullscreen: vm.customFullscreen // Only for -xs, -sm breakpoints.
-        })
-    };
-
-    vm.hide = function () {
-        $mdDialog.hide();
-    };
-
-    vm.cancel = function () {
-        $mdDialog.cancel();
-    };
-
-    vm.answer = function (answer) {
-        $mdDialog.hide(answer);
-    };
-
-    vm.addItemToShopList = function (storeId) {
-        var itemId = vm.shoppingListItemId;
-        AddItemService.addItemToShopList(storeId, itemId);
-    }
-
+    
     vm.verifyItemShopList = function (itemId) {
         var itemId = itemId;
         AddItemService.verifyItemShopList(itemId).then(function (response) {
@@ -108,6 +63,7 @@ myApp.controller('PantryViewController', function (UserService, UserSetupService
         })
     }
 
+    //triggers edit winodw from pantry view
     vm.editItemIndividual = function (item) {
         console.log('item in edit from pantry', item);
         
